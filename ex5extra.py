2660d8a28373328369ebe8a29b4bd6be27a5f1a6
tokyosuite <helenyueyu@gmail.com>
2019-02-17 13:24:00 -0800

    Finished ex5 extra.

---
+++ ex5extra.py
@@ -0,0 +1,16 @@
+print "I am going to convert my weight to centimeters and kilos."
+
+my_weight = 135
+my_height = 67
+
+print "I am %d pounds" % my_weight
+print "I am %d inches tall" % my_height
+
+print "1 pound is 0.453592 kg"
+print "1 inch is 2.54 centimeters"
+
+my_weight_kg = 135 * 0.453592
+my_height_cm = 67 * 2.54
+
+print "I am %d kg" % my_weight_kg
+print "I am %d cm tall" % my_height_cm
