e4bb978c1fc440f4ab5f2e7b45ec71f8d53e4a3d
tokyosuite <helenyueyu@gmail.com>
2019-02-17 13:29:07 -0800

    Finished up the fifth ex.

--- ex5extra.py
+++ ex5extra.py
@@ -14,3 +14,14 @@ my_height_cm = 67 * 2.54
 
 print "I am %d kg" % my_weight_kg
 print "I am %d cm tall" % my_height_cm
+
+print round(1.7333)
+
+my_name = 'The lonely programmer, Helen'
+my_age = 24 # Getting close to the quarter-life crisis!
+my_eyes = 'brown'
+my_hair = 'black'
+my_teeth = 'white'
+my_hobbies = 'playing the piano, finding cute coffee shops with wifi connection, attempting to try new cuisines on my spare time...'
+
+print "%s has the following hobbies: %s. She is %s years old, has %s eyes and %s hair, and her teeth are %s white, but maybe she shouldn't eat too much chocolate or they'll get yellow..." % (my_name, my_hobbies, my_age, my_eyes, my_hair, my_teeth)
