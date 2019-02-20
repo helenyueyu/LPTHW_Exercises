d63bf4d330c6bf155f9de0089f6b3b39e8144a66
tokyosuite <helenyueyu@gmail.com>
2019-02-19 17:46:01 -0800

    Added wicked Pride and Prejudice choose your own adventure story.

---
+++ ex31extra.py
@@ -0,0 +1,13 @@
+print "You are transported to Victorian England. You are now in a room with Mr. Darcy. What do you do? Do you..."
+print "1. Kiss him."
+print "2. Shoot him with a revolver."
+
+decision = raw_input("> ")
+
+if decision == "1": 
+  print "You have uncovered the secret to happiness! You are now Mrs. Darcy and will inherit Pemberley estate when your husband passes."
+
+else: 
+  print "Oops. You shot the hero of the romantic story. He was your one true love and you die a miserable old hag."
+
+
