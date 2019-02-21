5abd3b5496b1f1d9757c5f03da9a8c3247b8ac47
tokyosuite <helenyueyu@gmail.com>
2019-02-20 19:11:16 -0800

    Added additional functionality for number 4.

--- PracticePython/ex2.py
+++ PracticePython/ex2.py
@@ -1,9 +1,13 @@
 num = input("Enter a number: ")
-mod = num % 2
+mod = num % 4
 if mod == 0: 
+  print("Wow! Your number is also divisible by 4!")
+elif mod == 2: 
   print("You picked an even number.")
 else: 
   print("You picked an odd number.")
+
+
 
 
 
