3e217cea9c8f966534fcbaa561e7d252b02c1d0b
tokyosuite <helenyueyu@gmail.com>
2019-02-21 10:09:40 -0800

    Added.

---
+++ PythonCrashCourse/ex6.py
@@ -0,0 +1,16 @@
+favorite_pizzas = ['pineapple and ham', 'sausage and pepper', 'blue cheese and chicken']
+
+for pizza in favorite_pizzas:
+  print("I like " + pizza + " pizza.")
+
+print("I really love pizza!")
+
+
+australian_animals = ['kangaroo', 'koala', 'tarantula']
+
+for animal in australian_animals:
+  print("You might find a " + animal + " in the Australian outback.")
+
+print("Any of these animals would be scary to piss off!")
+
+
