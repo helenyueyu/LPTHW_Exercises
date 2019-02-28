367049c64eb0322aa1cb1699e91612e7b2232fd6
tokyosuite <helenyueyu@gmail.com>
2019-02-28 09:39:53 -0800

    Transformed health to bars. Also adding potion buying?

--- GeeksForGeeks/ex4.py
+++ GeeksForGeeks/ex4.py
@@ -2,7 +2,10 @@ import random
 
 your_health = 100
 opp_health = 100
+coins = 0
+potions = 0
 
+print("#"*50)
 print("You chose mage! You begin with 100 health")
 
 turn = 0;
@@ -10,23 +13,39 @@ turn = 0;
 while (your_health > 0 and opp_health > 0):
   turn += 1;
   if turn % 2 != 0:
-    choice = raw_input("Press enter to attack.")
+    choice = raw_input("Press enter to attack.\n Press 'b' to buy a potion.\n Press 't' to take a potion.")
     if choice == "":
       your_attack = random.randint(1,10)
       opp_health -= your_attack
-      print("You inflicted " + str(your_attack) + " damage! Your opponent's health is: " + str(opp_health))
+      coins += 1 if (random.randint(1,10) > 5) else 0
+      print(str(your_attack) + " damage!")
+      print("OPP HEALTH: " + str(opp_health/2*"#"))
+      print("GOLD: " + str(coins) + " coins")
+    elif choice == 'b':
+      if coins >= 10:
+        potions += 1
+      else: 
+        print("You're too poor to purchase potions!")
+    elif choice == 't':
+      if potions >= 1:
+        potions -= 1
+        potency = random.randint(10,15)
+        your_health += potency
+        print("You regained " + str(potency) + " health!") 
     else: 
       print("You ran away!")
       break
   else:
     opp_attack = random.randint(1,10)
     your_health -= opp_attack
-    print("Your opponent inflicted " + str(opp_attack) + " damage! Your health is: " + str(your_health))
+    print(str(opp_attack) + " damage!")
+    print("YOUR HEALTH: " + str(your_health/2*"#"))
 
 if your_health < 0:
   print("You lose!")
 
 if opp_health < 0:
   print("You win!")
+
 
 
